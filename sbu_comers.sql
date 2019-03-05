9fac665209fd02a68b827cf67f59ea45724d98b5
Aethan Matthew Ilagan <aethanilagan@gmail.com>
2019-03-05 11:25:32 +0800

    updated database

--- sbu_comers.sql
+++ sbu_comers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2019 at 03:11 AM
+-- Generation Time: Mar 05, 2019 at 03:33 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -83,6 +83,15 @@ CREATE TABLE `ci_sessions` (
   `data` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
+('odpablvo8mfbasvmgd6gc46asle5cahs', '::1', 1551755938, 0x5f5f63695f6c6173745f726567656e65726174657c693a313535313735353735343b),
+('rpugvmeuknj1bfjn83jq9vl204bbfe31', '::1', 1551756294, 0x5f5f63695f6c6173745f726567656e65726174657c693a313535313735363039393b6163636f756e745f69647c733a343a2242495453223b6f7267616e697a6174696f6e7c733a33363a22426564616e20496e666f726d6174696f6e20546563686e6f6c6f677920536f6369657479223b6c6f676765645f696e7c623a313b),
+('pcre4otemu4h1ad81ktu34lnt67jdoif', '::1', 1551756272, 0x5f5f63695f6c6173745f726567656e65726174657c693a313535313735363236313b6163636f756e745f69647c733a353a225053534255223b6f7267616e697a6174696f6e7c733a34313a2250737963686f6c6f677920536f6369657479206f662053616e204265646120556e6976657273697479223b6c6f676765645f696e7c623a313b);
+
 -- --------------------------------------------------------
 
 --
@@ -94,22 +103,23 @@ CREATE TABLE `registration_systems` (
   `Register_Name` varchar(15) NOT NULL,
   `Account_ID` varchar(20) NOT NULL,
   `Title` text NOT NULL,
-  `Logo` text NOT NULL
+  `Logo` text NOT NULL,
+  `Status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `registration_systems`
 --
 
-INSERT INTO `registration_systems` (`System_ID`, `Register_Name`, `Account_ID`, `Title`, `Logo`) VALUES
-('189291', 'Flap', 'PSSBU', 'General Assembly (Psych)', 'PSSBU_logo2.png'),
-('224942', 'Dog', 'BITS', 'ShareIT', 'BITS_Logo2.png'),
-('428051', 'Bamboo', 'BITS', 'hi', 'SBES_logo.jpg'),
-('514129', 'Chipotle', 'HRDMS', 'Yoshi: The HUMAN ZONE', 'HRDMS1.png'),
-('622509', 'Dog', 'PSSBU', 'Yoga', 'PSSBU_logo1.png'),
-('719821', 'Bamboo', 'SBES', 'Economic Zones', 'SBU.png'),
-('801339', 'Dog', 'JFINMA', 'Body Wars', 'SC_logo.png'),
-('904611', 'Ellie', 'HRDMS', 'Human Resources Zone 101', 'HRDMS.png');
+INSERT INTO `registration_systems` (`System_ID`, `Register_Name`, `Account_ID`, `Title`, `Logo`, `Status`) VALUES
+('189291', 'Flap', 'PSSBU', 'General Assembly (Psych)', 'PSSBU_logo2.png', 0),
+('224942', 'Dog', 'BITS', 'ShareIT', 'BITS_Logo2.png', 0),
+('428051', 'Bamboo', 'BITS', 'hi', 'SBES_logo.jpg', 0),
+('514129', 'Chipotle', 'HRDMS', 'Yoshi: The HUMAN ZONE', 'HRDMS1.png', 0),
+('622509', 'Dog', 'PSSBU', 'Yoga', 'PSSBU_logo1.png', 0),
+('719821', 'Bamboo', 'SBES', 'Economic Zones', 'SBU.png', 0),
+('801339', 'Dog', 'JFINMA', 'Body Wars', 'SC_logo.png', 0),
+('904611', 'Ellie', 'HRDMS', 'Human Resources Zone 101', 'HRDMS.png', 0);
 
 -- --------------------------------------------------------
 
